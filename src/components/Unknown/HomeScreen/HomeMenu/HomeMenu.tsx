690115977cda3f8d675fa347ff36c2222f4df527
Dmitriy <bmd4work@gmail.com>
2022-03-07 00:09:24 +0600

    feat: realize logout logic

--- src/components/Unknown/HomeScreen/HomeMenu/HomeMenu.tsx
+++ src/components/Unknown/HomeScreen/HomeMenu/HomeMenu.tsx
@@ -1,9 +1,19 @@
-import { AppBar, Avatar, IconButton, Toolbar, Typography } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Menu,
+  Button,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 import React from 'react';
 import { createStyles, makeStyles } from '@mui/styles';
 import { Theme } from '@mui/system';
 import { useUser } from 'reactfire';
+import { auth } from '../../../../common/firebaseApp';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,6 +35,21 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function HomeMenu() {
   const classes = useStyles();
   const user = useUser();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  async function logout() {
+    handleClose();
+    await auth.signOut();
+  }
+
   function extractInitials(fullName: string) {
     const arr = fullName.split(' ');
     return arr[0][0] + arr[1][0];
@@ -43,12 +68,24 @@ export default function HomeMenu() {
             color="inherit"
             aria-label="menu"
           >
-            <Menu />
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Voypost
           </Typography>
-          <Avatar>{initials}</Avatar>
+          <Button onClick={handleClick}>
+            <Avatar>{initials}</Avatar>
+          </Button>
+
+          <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={logout}> Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </div>
