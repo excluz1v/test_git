992f7b5f196b768390eb0813e84727cf94e10610
Dmitriy <bmd4work@gmail.com>
2022-03-07 17:09:03 +0600

    feat: create password length constants

--- src/components/Auth/SignInScreen/LoginForm/validateSchema.ts
+++ src/components/Auth/SignInScreen/LoginForm/validateSchema.ts
@@ -1,9 +1,13 @@
 import * as Yup from 'yup';
+import {
+  MAX_PASSWORD_LENGTH,
+  MIN_PASSWORD_LENGTH,
+} from '../../../../common/constants';
 
 const LoginSchema = Yup.object().shape({
   password: Yup.string()
-    .min(2, 'Слишком короткий пароль!')
-    .max(20, 'Слишком длинный пароль')
+    .min(MIN_PASSWORD_LENGTH, 'Слишком короткий пароль!')
+    .max(MAX_PASSWORD_LENGTH, 'Слишком длинный пароль')
     .required('Обязательное поле'),
   email: Yup.string().email('Некорректный email').required('Обязательное поле'),
 });
