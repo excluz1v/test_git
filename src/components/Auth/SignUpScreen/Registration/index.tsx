32173fdfa47c30f1d77ee43a5ed6f1742148c45b
Dmitriy <bmd4work@gmail.com>
2022-03-08 10:25:45 +0600

    feat: create Registration component

--- src/components/Auth/SignUpScreen/Registration/index.tsx
+++ src/components/Auth/SignUpScreen/Registration/index.tsx
@@ -1,7 +1,29 @@
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
+import Logo from '../../SignInScreen/Logo/Logo';
+import useStyles from './style';
 
-function Registration() {
-  return <div>index</div>;
-}
+const Registration: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      className={classes.wrapper}
+      spacing={5}
+    >
+      <Grid justifyContent="center" item xs={12}>
+        <Logo />
+      </Grid>
+      <Grid justifyContent="center" item xs={12}>
+        <Typography variant="h2">Login</Typography>
+      </Grid>
+
+      <Grid justifyContent="center" item xs={12} />
+    </Grid>
+  );
+};
 
 export default Registration;
