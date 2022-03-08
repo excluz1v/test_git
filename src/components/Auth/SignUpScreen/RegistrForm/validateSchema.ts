9f7c7ca42a6b143e97e8821044a17cc9d631c785
Dmitriy <bmd4work@gmail.com>
2022-03-08 11:11:34 +0600

    fix: add dynamic validation message

--- src/components/Auth/SignUpScreen/RegistrForm/validateSchema.ts
+++ src/components/Auth/SignUpScreen/RegistrForm/validateSchema.ts
@@ -4,12 +4,16 @@ import {
   MIN_PASSWORD_LENGTH,
 } from '../../../../common/constants';
 
-const LoginSchema = Yup.object().shape({
+const RegisterSchema = Yup.object().shape({
   password: Yup.string()
     .min(MIN_PASSWORD_LENGTH, `at least ${MIN_PASSWORD_LENGTH} characters`)
     .max(MAX_PASSWORD_LENGTH, `${MAX_PASSWORD_LENGTH} characters is maximum`)
     .required('Required field'),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref('password')],
+    ' password must be the same',
+  ),
   email: Yup.string().email('invalid email').required('Required field'),
 });
 
-export default LoginSchema;
+export default RegisterSchema;
